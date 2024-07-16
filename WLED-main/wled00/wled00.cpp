f52df9f4210eaf10fb7c4531218a4e0697e6df4e
GautamVhavle <gautamvhavle@gmail.com>
2024-07-16 13:22:18 +0530

    successful integration with golain platform

--- WLED-main/wled00/wled00.cpp
+++ WLED-main/wled00/wled00.cpp
@@ -1,74 +1,74 @@
-/*
- * WLED Arduino IDE compatibility file.
- * 
- * Where has everything gone?
- * 
- * In April 2020, the project's structure underwent a major change. 
- * Global variables are now found in file "wled.h"
- * Global function declarations are found in "fcn_declare.h"
- * 
- * Usermod compatibility: Existing wled06_usermod.ino mods should continue to work. Delete usermod.cpp.
- * New usermods should use usermod.cpp instead.
- */
-
 #include <Arduino.h>
-#include <shadow.pb.h>
+#include "wled.h"
 
-#include<certs.h>
+#include "certs.h"
+
 #ifndef DEVICE_NAME 
 #error "Please include the correct certs.h file."
 #endif
+
+#define LED_BUILTIN 2
+#include "shadow.pb.h"
+
+
+#include <WiFi.h>
 #include <WiFiClientSecure.h>
-#include "wled.h"
 
 // NOTE: Import everything else before Golain.h
 #include "Golain.h"
 
-Golain golain(GOLAIN_CERT_CA, GOLAIN_DEVICE_CERT, GOLAIN_PRIVATE_KEY);
+
+
+
+Golain golain(DEVICE_NAME, ROOT_TOPIC, GOLAIN_CERT_CA, GOLAIN_DEVICE_CERT, GOLAIN_PRIVATE_KEY);
 
 void onDeviceShadowUpdate(Shadow current, Shadow last) {
     Serial.println("Device shadow updated");
 
     // Control the LED based on the shadow state
     if (current.on) {
-      Serial.println("Toggled LED");
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(1000);
+        Serial.println("LED is ON");
+
     } else {
-      Serial.println("Toggled LED");
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(1000);
+        Serial.println("LED is OFF");
     }
-    // Serial output char data type given from the console
-    // if(current.has_macAddr){
-    //   Serial.println(current.macAddr);
-    // }
 }
 
-
 void setup() {
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  WiFi.begin("Quoppo Ventures ", "Quopeupp");
+    // Fill in your Wi-Fi credentials here to connect to the internet:
+    WiFi.begin("Quoppo Ventures ", "Quopeupp");
 
-  while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-  }
-
-  Serial.println("Connected to WiFi");
-
-
-  golain.setDeviceShadowCallback(&onDeviceShadowUpdate);
-
-  golain.begin();
-  WLED::instance().setup();
-  }
-
-void loop() {
-  golain.loop();
-  WLED::instance().loop();
-
-  if (millis() % 5000 == 0) {
-      // GlobalShadow.buttonPressedCount = 30;
-      golain.updateShadow();
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
     }
 
-  delay(1000);
+    Serial.println("Connected to WiFi");
+
+    golain.setDeviceShadowCallback(&onDeviceShadowUpdate);
+
+    golain.begin();
+    WLED::instance().setup();
+
+
+    pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() {
+    // Call this function to keep the connection alive.
+    golain.loop();
+    WLED::instance().loop();
+
+    if (millis() % 5000 == 0) {
+        GlobalShadow.buttonPressedCount = 10;
+        golain.updateShadow();
+    }
+    delay(1000);
 }
