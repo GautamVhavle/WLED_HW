4b84becf09b695eff2e56d0a7225ad07851d5396
GautamVhavle <gautamvhavle@gmail.com>
2024-08-01 23:25:37 +0530

    add QLED usermod

--- WLED-main/wled00/usermod_v2_golain.h
+++ WLED-main/wled00/usermod_v2_golain.h
@@ -4,7 +4,7 @@
 
 //This is an empty v2 usermod template. Please see the file usermod_v2_example.h in the EXAMPLE_v2 usermod folder for documentation on the functions you can use!
 
-class UsermodRenameMe : public Usermod {
+class QLED : public Usermod {
   private:
 
   public:
--- WLED-main/wled00/wled00.cpp
+++ WLED-main/wled00/wled00.cpp
@@ -1,74 +1,21 @@
-#include <Arduino.h>
+/*
+ * WLED Arduino IDE compatibility file.
+ * 
+ * Where has everything gone?
+ * 
+ * In April 2020, the project's structure underwent a major change. 
+ * Global variables are now found in file "wled.h"
+ * Global function declarations are found in "fcn_declare.h"
+ * 
+ * Usermod compatibility: Existing wled06_usermod.ino mods should continue to work. Delete usermod.cpp.
+ * New usermods should use usermod.cpp instead.
+ */
 #include "wled.h"
 
-#include "certs.h"
-
-#ifndef DEVICE_NAME 
-#error "Please include the correct certs.h file."
-#endif
-
-#define LED_BUILTIN 2
-#include "shadow.pb.h"
-
-
-#include <WiFi.h>
-#include <WiFiClientSecure.h>
-
-// NOTE: Import everything else before Golain.h
-#include "Golain.h"
-
-
-
-
-Golain golain(DEVICE_NAME, ROOT_TOPIC, GOLAIN_CERT_CA, GOLAIN_DEVICE_CERT, GOLAIN_PRIVATE_KEY);
-
-void onDeviceShadowUpdate(Shadow current, Shadow last) {
-    Serial.println("Device shadow updated");
-
-    // Control the LED based on the shadow state
-    if (current.on) {
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(1000);
-        Serial.println("LED is ON");
-
-    } else {
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(1000);
-        Serial.println("LED is OFF");
-    }
-}
-
 void setup() {
-    Serial.begin(115200);
-
-    // Fill in your Wi-Fi credentials here to connect to the internet:
-    WiFi.begin("Quoppo Ventures ", "Quopeupp");
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-
-    Serial.println("Connected to WiFi");
-
-    golain.setDeviceShadowCallback(&onDeviceShadowUpdate);
-
-    golain.begin();
-    WLED::instance().setup();
-
-
-    pinMode(LED_BUILTIN, OUTPUT);
-
+  WLED::instance().setup();
 }
 
 void loop() {
-    // Call this function to keep the connection alive.
-    golain.loop();
-    WLED::instance().loop();
-
-    if (millis() % 5000 == 0) {
-        GlobalShadow.buttonPressedCount = 10;
-        golain.updateShadow();
-    }
-    delay(1000);
+  WLED::instance().loop();
 }
